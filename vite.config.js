3952fd868303306dc5232b184a94131268c72af2
rickshaw68 <rickshaw@myyahoo.com>
2025-06-12 20:38:19 -0600

    update file paths in vite to work in netlify

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: './' 
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        favorites: resolve(__dirname, 'favorites.html'),
+        grocery: resolve(__dirname, 'grocery.html'),
+        recipe: resolve(__dirname, 'recipe.html')
+      }
+    }
+  }
 })
